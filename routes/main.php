ec9e17dd98004303ff09a2af230162c28bbef7c1
dzoom <dzoomfeed142@gmail.com>
2020-02-29 21:33:01 +0300

    update main

--- routes/main.php
+++ routes/main.php
@@ -1,42 +1,8 @@
 <?php
 $path ="/";
+require_once("./temp/head.php");
+require_once("./temp/nav.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MA - Главная</title>
-    <script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="/static/css/main">
-</head>
-<body>
-    <nav>
-        <div class="nav-wrapper">
-            <div class="nav-logo">
-                <a href="#"><img src="/static/png/logo" width="100%" alt="Navigation Logo"></a>
-            </div>
-            <div class="nav-buttons">
-                <a class="nav-button" href="#">Возможности</a>
-                <a class="nav-button" href="#">О проекте</a>
-                <a class="nav-button" href="#">Контакты</a>
-            </div>
-            <div class="nav-b-login">
-                <a href="">Войти</a>
-            </div>
-        </div>
-    </nav>
-    <div class="l-form-wrapper">
-        <form action="/auth/login" class="l-form">
-            <input type="text" name="name" placeholder="Логин">
-            <input type="password" name="password" placeholder="Пароль">
-            <input type="submit" value="Войти">
-        </form>    
-    </div>
     <div class="main">
         <div class="main-landing">
             <div class="landing-wrapper">
