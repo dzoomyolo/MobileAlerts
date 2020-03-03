8f09f94124b3fc215ed6356a031f7658e3a45ae5
dzoom <dzoomfeed142@gmail.com>
2020-03-03 17:17:45 +0300

    add 404

---
+++ routes/404.php
@@ -0,0 +1,11 @@
+<?php
+$path = "/404";
+require_once("./temp/head.php");
+require_once("./temp/nav.php");
+?>
+<div class="404-img" style="background-color:#232323;">
+    <img style="user-select:none;pointer-events:none;" src="/static/png/mai404" width="100%" alt="">
+</div>
+<?php
+require_once("./temp/footer.php");
+?>
