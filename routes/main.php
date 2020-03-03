8d761eafd3e0715f232ca5150bee8b0f86af6f31
dzoom <dzoomfeed142@gmail.com>
2020-03-03 17:23:06 +0300

    set footer and update buttons

--- routes/main.php
+++ routes/main.php
@@ -3,43 +3,44 @@ $path ="/";
 require_once("./temp/head.php");
 require_once("./temp/nav.php");
 ?>
-    <div class="main">
-        <div class="main-landing">
-            <div class="landing-wrapper">
-                <h2 style="color:#7a7a7a;"><span style="color: #31ad52;">Mobile</span>Alerts</h2>
-                <h5 style="color:#7a7a7a;font-size: 1em; font-size:1.13em; ">Лучшее решение для школ!</h5>
-                <div class="btn-group" role="group">
-                    <a class="btn btn-outline-dark" href="/">Подключить школу</a>
-                    <a class="btn btn-outline-dark" href="/me">Личный кабинет</a>
-                </div>
-            </div>
-        </div>
-        <div class="main-features">
-            <div class="f-card">
-                <div class="f-image">
-                    <img src="/static/png/phone" width="100%" alt="Card features">
-                </div>
-                <div class="f-card-body">
-                    <span>Много новых штукdasdsadsadasdasdasdadasdsa<span>
-                </div>
-            </div>
-            <div class="f-card">
-                <div class="f-image">
-                    <img src="/static/png/phone" width="100%" alt="Card features">
-                </div>
-                <div class="f-card-body">
-                    <span>Много новых штукdasdsadsadasdasdasdadasdsa<span>
-                </div>
-            </div>
-            <div class="f-card">
-                <div class="f-image">
-                    <img src="/static/png/phone" width="100%" alt="Card features">
-                </div>
-                <div class="f-card-body">
-                    <span>Много новых штукdasdsadsadasdasdasdadasdsa<span>
-                </div>
+<div class="main">
+    <div class="main-landing">
+        <div class="landing-wrapper">
+            <h2 style="color:#7a7a7a;"><span style="color: #31ad52;">Mobile</span>Alerts</h2>
+            <h5 style="color:#7a7a7a;font-size: 1em; font-size:1.13em; ">Лучшее решение для школ!</h5>
+            <div class="btn-group" role="group">
+                <a class="btn btn-outline-dark" href="/">Подключить школу</a>
+                <a class="btn btn-outline-dark" id="main-login" href="/me">Личный кабинет</a>
             </div>
         </div>
     </div>
-</body>
-</html>
+    <div class="main-features">
+        <div class="f-card">
+            <div class="f-image">
+                <img src="/static/png/phone" width="100%" alt="Card features">
+            </div>
+            <div class="f-card-body">
+                <span>Много новых штукdasdsadsadasdasdasdadasdsa<span>
+            </div>
+        </div>
+        <div class="f-card">
+            <div class="f-image">
+                <img src="/static/png/phone" width="100%" alt="Card features">
+            </div>
+            <div class="f-card-body">
+                <span>Много новых штукdasdsadsadasdasdasdadasdsa<span>
+            </div>
+        </div>
+        <div class="f-card">
+            <div class="f-image">
+                <img src="/static/png/phone" width="100%" alt="Card features">
+            </div>
+            <div class="f-card-body">
+                <span>Много новых штукdasdsadsadasdasdasdadasdsa<span>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+require_once("./temp/footer.php");
+?>
