b0b2f4db4e5bfbda0fe6ced8192ac494684abfac
dzoom <dzoomfeed142@gmail.com>
2020-03-10 18:44:26 +0300

    update profile

--- routes/me.php
+++ routes/me.php
@@ -5,66 +5,12 @@ $user = qPost("/api/users/get","token=".$_SESSION['token']);
 $user = $user->user;
 require_once("./temp/head.php");
 require_once("./temp/nav.php");
-?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.19/jquery.touchSwipe.min.js"></script>
-<div class="profile">
-    <div class="p-wrapper">
-        <div class="p-logo">
-            <img style="pointer-events:none;" src="/static/png/avatar" width="100%" alt="">
-        </div>
-        <div class="p-info">
-            <span>Пользователь: <?=$user->lastname?> <?=$user->name?> <?=$user->middlename?></span>
-        </div>
-    </div>
-    <div class="alerts-wrapper">
-        <h2>Уведомления</h2>
-        <div class="alerts-scroll">
-        </div>
-    </div>
-</div>
-
-<script>
-(()=>{fetchAlerts()})();
-async function getAlerts(token){
-    let alertsAnswer = await fetch('/api/users/alerts', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: `token=${token}`
-    });
-    let alerts = await alertsAnswer.json();
-    return Promise.resolve(alerts);
+if($user->privileges == 1){
+    require_once("./temp/user.php");
+}else if($user->privileges == 2){
+    require_once("./temp/teacher.php");
+}else{
+    echo "ОБРАТИТЕСЬ К СВОЕМУ АДМИНИСТРАТОРУ, ПРИВИЛЕГИЯ НЕ ОПРЕДЕЛЕНА; PLEASE CONTACT YOUR ADMINISTRATOR, PRIVILEGES NOT DETERMINED";
 }
-function fetchAlerts(){
-    getAlerts(token).then(a=>{
-        $('.alerts-scroll').empty();
-        a.alerts.map(e=>{
-        $('.alerts-scroll').append(`
-            <div class="alert">
-                <div class="alert-content">
-                    <div class="alert-header">
-                        <h3>${e.title}</h3>
-                    </div>
-                    <div class="alert-body">
-                        <span>${e.message}</span>
-                    </div>
-                </div>
-            </div>`);    
-        })
-    })
-}
-$(".alert").on("swipeleft",()=>{
-    console.log("Swipe left side");
-    $(".alert").swipe({swipeLeft:(()=>{
-        console.log("Swipe left side");
-    })});
-})
-// setInterval(() => {
-//     fetchAlerts();
-// }, 5000);
-</script>
-
-<?php
 require_once("./temp/footer.php");
 ?>
