1eb579efff1b448832030682cc5238f4a3d570be
dzoom <dzoomfeed142@gmail.com>
2020-03-10 18:44:04 +0300

    get childrens api method

--- core/apiMethods/users.php
+++ core/apiMethods/users.php
@@ -33,7 +33,7 @@ if($params[3]=="auth"){
     }
     $l = $db->query('SELECT `id` FROM `user` WHERE `ref_token`= :token',array("token"=>$t));
     if(!is_empty($l['id'])){
-        $user = $db->query('SELECT `id`,`name`,`lastname`,`middlename`,`privileges` FROM `user` WHERE `id`= :id',array("id"=>$l['id']));
+        $user = $db->query('SELECT `id`,`name`,`lastname`,`middlename`,`privileges`,`shcool_id` FROM `user` WHERE `id`= :id',array("id"=>$l['id']));
         $uA = (object)array();
         $this->answer->user = $user;
     }else{
@@ -48,6 +48,23 @@ if($params[3]=="auth"){
     if(!is_empty($l['id'])){
         $alerts = $db->queryAll('SELECT * FROM `alerts` WHERE `receiver`= :id ORDER BY `id` DESC',array("id"=>$l['id']));
         $this->answer->alerts = $alerts;
+    }else{
+        $this->createError(403,"Invalid Token");
+    }
+}else if($params[3]=="childrens"){
+    $t = htmlspecialchars($_POST['token']);
+    $s = htmlspecialchars($_POST['s']);
+    if(is_empty($t)){
+        $this->createError(400,"Not enought parameters");
+    }
+    $l = $db->query('SELECT `id`,`privileges` FROM `user` WHERE `ref_token`= :token',array("token"=>$t));
+    if(!is_empty($l['id'])){
+        if($l['privileges'] >=2){
+            $u = $db->queryAll('SELECT `id`,`name`,`lastname`,`middlename` FROM `user` WHERE `shcool_id`= :s',array("s"=>$s));
+            $this->answer->users = $u;
+        }else{
+            $this->createError(403,"Not enough privileges");
+        }
     }else{
         $this->createError(403,"Invalid Token");
     }
